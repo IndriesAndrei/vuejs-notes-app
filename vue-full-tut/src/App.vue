af292facf56a9eb0f2b7b5126c002102e58c2997
IndriesAndrei <indriesandrei@yahoo.co.uk>
2023-07-25 13:16:30 +0300

    Complete the app and add some validation

--- vue-full-tut/src/App.vue
+++ vue-full-tut/src/App.vue
@@ -1,16 +1,173 @@
+<script setup>
+import { ref } from "vue";
+
+const showModal = ref(false);
+
+// initial state of a note is empty note
+const newNote = ref("");
+
+// show error if note text less than 10 characters
+const errorMessage = ref("");
+
+// initial state of notes is empty array of notes
+const notes = ref([]);
+
+// get light random colors for notes background-color
+function getRandomColor() {
+  return "hsl(" + Math.random() * 360 + ", 100%, 75%)";
+}
+
+const addNote = () => {
+  // note should have 10 characters or more to be valid
+  if (newNote.value.length > 9) {
+    notes.value.push({
+      id: Math.floor(Math.random() * 1000000),
+      text: newNote.value,
+      date: new Date(),
+      backgroundColor: getRandomColor(),
+    });
+    //close the modal after adding a note
+    showModal.value = false;
+    newNote.value = "";
+  }
+
+  return errorMessage.value = "Note needs to be 10 characters or more!";
+}
+</script>
+
 <template>
   <main>
+    <div v-if="showModal" class="overlay">
+      <div class="modal">
+        <textarea 
+          v-model="newNote" 
+          name="note" 
+          id="note" 
+          cols="30" 
+          rows="10"
+        ></textarea>
+        <p v-if="errorMessage">{{ errorMessage }}</p>
+        <button @click="addNote">Add Note</button>
+        <button class="close-button" @click="showModal = false">Close</button>
+      </div>
+    </div>
     <div class="container">
       <header>
         <h1>Notes</h1>
-        <button>+</button>
+        <button class="header-button" @click="showModal = true">+</button>
       </header>
       <div class="cards-container">
-        <div class="card">
-          <p class="main-text"></p>
-          <p class="date"></p>
+        <div 
+          v-for="note in notes" 
+            class="card" 
+            :style="{backgroundColor: note.backgroundColor}"
+            :key="note.id"
+          >
+          <p class="main-text">{{ note.text }}</p>
+          <p class="date">{{ note.date.toLocaleDateString("en-US") }}</p>
         </div>
       </div>
     </div>
   </main>
 </template>
+
+<style scoped>
+main {
+  height: 100vh;
+  width: 100vw;
+}
+
+.container {
+  max-width: 1000px;
+  padding: 10px;
+  margin: 0 auto;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h1 {
+  font-weight: bold;
+  margin-bottom: 25px;
+  font-size: 75px;
+}
+
+.header-button {
+  border: none;
+  padding: 10px;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  background-color: lightblue;
+  border-radius: 100%;
+  font-size: 20px;
+}
+
+/* card styles */
+.card {
+  width: 225px;
+  height: 225px;
+  background-color: yellow;
+  padding: 10px;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-right: 20px;
+  margin-bottom: 20px;
+}
+
+.date {
+  font-size: 12.5px;
+  font-weight: bold;
+}
+
+.cards-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+/* modal styles */
+.overlay {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.77);
+  z-index: 10;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.modal {
+  width: 750px;
+  background-color: white;
+  border-radius: 10px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+}
+
+.modal button {
+  padding: 10px 20px;
+  font-size: 20px;
+  width: 100%;
+  background-color: blueviolet;
+  border: none;
+  color: white;
+  cursor: pointer;
+  margin-top: 15px;
+}
+
+.modal .close-button {
+  background-color: rgb(193, 15, 15);
+}
+
+.modal p {
+  color: red;
+}
+</style>
